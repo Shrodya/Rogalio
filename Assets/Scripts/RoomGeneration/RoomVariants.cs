3157e7705d7b376f07e23ed046595bcf850efea8
Shrodya <rodyaestgovno@gmail.com>
2023-12-07 21:33:27 +0300

    FirstTry

---
+++ Assets/Scripts/RoomGeneration/RoomVariants.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomVariants : MonoBehaviour
+{
+    public GameObject[] RoomWithWalls;
+    public GameObject[] RoomWithFountain;
+    public GameObject[] RoomWithMage;
+    public GameObject[] EmptyRoom;
+}
